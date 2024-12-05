c8aeca65a14de556dd8d48635c1537a8985cb08f
Bohdan Khokhlov <khokhlov2025mf12@student.karazin.ua>
2024-12-05 12:18:10 +0300

    spark_invert_index

--- Spark/src/main/scala/main.scala
+++ Spark/src/main/scala/main.scala
@@ -14,13 +14,66 @@ object main {
       .config("spark.driver.extraJavaOptions", "--add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED")
       .getOrCreate()
     val sc = spark.sparkContext
-    val graph = GraphLoader.edgeListFile(sc, "E:\\future generations\\4 course 1 semester\\graph к ЛР 3.txt", canonicalOrientation = false)
-        .partitionBy(PartitionStrategy.RandomVertexCut)
+//    val graph = GraphLoader.edgeListFile(sc, "E:\\future generations\\4 course 1 semester\\graph к ЛР 3.txt", canonicalOrientation = false)
+//        .partitionBy(PartitionStrategy.RandomVertexCut)
+//
+//        val triCounts = graph.triangleCount().vertices
+//
+//        val totalTriangles = triCounts.map(_._2).reduce(_ + _) / 3
+//        println(s"Total number of triangles: $totalTriangles")
+    import spark.implicits._
 
-        val triCounts = graph.triangleCount().vertices
+    val dataPath = "E:\\spark_lab6\\20_newsgroup"
 
-        val totalTriangles = triCounts.map(_._2).reduce(_ + _) / 3
-        println(s"Total number of triangles: $totalTriangles")
+    def cleanText(text: String): String = {
+      text.toLowerCase
+        .replaceAll("(?i)path:.*", "")
+        .replaceAll("(?i)newsgroups:.*", "")
+        .replaceAll("(?i)writes:.*", "")
+        .replaceAll("(?i)from:.*", "")
+        .replaceAll("(?i)lines:.*", "")
+        .replaceAll("(?i)date:.*", "")
+        .replaceAll("(?i)References:.*", "")
+        .replaceAll("(?i)Organization:.*", "")
+        .replaceAll("(?i)Nntp-Posting-Host:.*", "")
+        .replaceAll("(?i)Sender.*", "")
+        .replaceAll("(?i)Message-ID.*", "")
+        .replaceAll("(?i)Xref.*", "")
+        .replaceAll("'","\'")
+        .replaceAll("(?is)begin\\s+\\d+\\s+\\S+.*?end", "")
+        .replaceAll("[^a-z']", " ")
+    }
+
+    val files = sc.wholeTextFiles(dataPath + "/*")
+      .map { case (filePath, content) =>
+        val filename = filePath.split("/").last.split("\\.").head
+        val cleanedContent = cleanText(content)
+        (filename, cleanedContent)
+      }.toDF("docId", "content")
+
+
+    val words = files
+      .select($"docId", explode(split($"content", "\\s+")).as("word"))
+      .filter($"word".rlike("^[a-z']+$") && !$"word".startsWith("\'"))
+
+
+    val invertedIndex = words
+      .groupBy("word")
+      .agg(
+        count("*").as("total_count"),
+        collect_set("docId").as("documents")
+      )
+      .select(
+        $"word",
+        $"total_count",
+        concat_ws(" ", $"documents").as("doc_list")
+      )
+
+    invertedIndex.coalesce(1)
+      .write
+      .mode("overwrite")
+      .option("header", "true")
+      .csv("E:\\spark_lab6\\output_data")
 
     spark.stop()
   }
